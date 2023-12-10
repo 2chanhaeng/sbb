f90b5c73ff31b3f19dfc41a7bb790612e8f27033
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-10 16:49:11 +0900

    create MainController

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,15 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index() {
+        return "Sensitively takin', 내 취향 저격의 taste";
+    }
+}
