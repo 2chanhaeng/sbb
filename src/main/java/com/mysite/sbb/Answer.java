5b47acb9574a8628c22be735775398aad3356309
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-10 17:01:31 +0900

    set Answer.question as many to one

--- src/main/java/com/mysite/sbb/Answer.java
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -24,5 +25,6 @@ public class Answer {
 
     private LocalDateTime createDate;
 
+    @ManyToOne
     private Question question;
 }
