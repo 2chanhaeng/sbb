44e16a2d3345ccbaf3dc7cfd4f1e62622177e08d
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-13 19:58:01 +0900

    create question read controller

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,8 +15,14 @@ public class QuestionController {
     private final QuestionRepository questionRepository;
 
     @GetMapping("/question/list")
-    public List<Question> list(Model model) {
+    public List<Question> list() {
         List<Question> questionList = this.questionRepository.findAll();
         return questionList;
+    }
+
+    @GetMapping("/question/details/{id}")
+    public Question detail(@PathVariable Integer id, Model model) {
+        Question question = this.questionRepository.findById(id).orElse(null);
+        return question;
     }
 }
