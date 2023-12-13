e41c7893d7c0c38e243bdae897ed28315fbbeb5a
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-13 20:21:07 +0900

    apply RequestMapping

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,25 +4,27 @@ import com.mysite.sbb.DataNotFoundException;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @RestController
 public class QuestionController {
     private final QuestionRepository questionRepository;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public List<Question> list() {
         List<Question> questionList = this.questionRepository.findAll();
         return questionList;
     }
 
-    @GetMapping("/question/details/{id}")
+    @GetMapping("/details/{id}")
     public Question detail(@PathVariable Integer id, Model model) {
         Question question = this.questionRepository.findById(id)
                 .orElseThrow(() -> new DataNotFoundException("Question not found with id " + id));
