46370c7ae4d1953b41faa7080ef7934c61c13dbf
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-13 19:37:21 +0900

    make /question/list response Question list

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,16 +1,21 @@
 package com.mysite.sbb.question;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
 public class QuestionController {
+    private final QuestionRepository questionRepository;
 
     @GetMapping("/question/list")
-    @ResponseBody
-    public String list() {
-        return "question/list";
+    public List<Question> list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        return questionList;
     }
-
 }
