b84fc50bd2eac377afaf771c09de2031149f956b
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-13 22:21:44 +0900

    create controller to create answer

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -1,5 +1,29 @@
 package com.mysite.sbb.answer;
 
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@RestController
 public class AnswerController {
 
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+
+    @PostMapping("/create/{id}")
+    public void createAnswer(
+            @PathVariable("id") Integer id,
+            @RequestBody AnswerCreateDTO answer) {
+        Question question = this.questionService.getQuestion(id);
+        this.answerService.create(question, answer);
+    }
 }
