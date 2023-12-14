e4eaa7a46570d581486ef498c6f7bf3f0332a3eb
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-14 20:35:01 +0900

    create user signup controller

--- src/main/java/com/mysite/sbb/user/UserController.java
+++ src/main/java/com/mysite/sbb/user/UserController.java
@@ -1,4 +1,36 @@
 package com.mysite.sbb.user;
 
+import jakarta.validation.Valid;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public String signup(@Valid UserCreateDTO userCreateDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+
+        if (!userCreateDTO.getPassword1().equals(userCreateDTO.getPassword2())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect",
+                    "2개의 패스워드가 일치하지 않습니다.");
+            return "signup_form";
+        }
+
+        userService.create(userCreateDTO.getUsername(),
+                userCreateDTO.getEmail(), userCreateDTO.getPassword1());
+
+        return "redirect:/";
+    }
 }
