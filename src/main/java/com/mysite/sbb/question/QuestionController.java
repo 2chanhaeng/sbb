085fe641173998cb7b4f31b40d654a92f05e40b4
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-14 18:10:40 +0900

    apply question reads pagination

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Page;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,8 +24,8 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/list")
-    public List<Question> list() {
-        List<Question> questionList = this.questionService.getList();
+    public Page<Question> list(@RequestParam(value = "page", defaultValue = "0") int page) {
+        Page<Question> questionList = this.questionService.getList(page);
         return questionList;
     }
 
