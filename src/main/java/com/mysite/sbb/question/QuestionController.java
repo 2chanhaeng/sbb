8df32bed9ec54355e63000e96614f1a9314e9e32
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-14 16:11:16 +0900

    create /question/create controller

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,12 +1,15 @@
 package com.mysite.sbb.question;
 
 import com.mysite.sbb.DataNotFoundException;
+import com.mysite.sbb.answer.AnswerCreateDTO;
 
 import java.util.List;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +30,16 @@ public class QuestionController {
     @GetMapping("/details/{id}")
     public Question detail(@PathVariable Integer id, Model model) {
         return this.questionService.getQuestion(id);
+    }
+
+    @PostMapping("/create")
+    public QuestionCreatedDTO createQuestion(@RequestBody QuestionCreateDTO questionDto) {
+        try {
+            Question question = questionDto.toEntity();
+            this.questionService.create(question);
+            return question.toCreatedDTO();
+        } catch (Exception e) {
+            throw new DataNotFoundException("Question not created.");
+        }
     }
 }
